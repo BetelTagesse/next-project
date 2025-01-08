406f127e483a2f671fb0d62035e3bd66b1b5dbce
BetelTagesse <BetelTagesse367@gmail.com>
2025-01-08 14:53:56 +0300

    browse done

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,27 @@
-export default function MealsPage() {
-  return <div>MealsPage</div>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+export default async function MealsPage() {
+  await 
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+
+        <p className={classes.cta}>
+          <Link href="/meals/share">share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,14 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
 import NavLink from "./nav-link";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <>
       <MainHeaderBackground />
