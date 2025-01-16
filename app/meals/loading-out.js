788efc91af762fd1ba7da9e7bbd3784a299809c5
BetelTagesse <BetelTagesse367@gmail.com>
2025-01-16 19:22:54 +0300

    share

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,4 +1,4 @@
 import classes from "./loading.module.css";
 export default function MealsLoadingPage() {
-  return <p className={classes.loading}>Loading ...</p>;
+  return <p className={classes.loading}>Fetching meals ...</p>;
 }
